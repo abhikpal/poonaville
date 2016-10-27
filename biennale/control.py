24075d6d73ecdc6f57bd5542f114791b32aad336
Abhik Pal <theabhikpal@gmail.com>
2016-10-27 14:28:15 +0530

    added controller ui from prototype 1

--- biennale/control.py
+++ biennale/control.py
@@ -3,6 +3,4 @@ from biennale import socketio
 @socketio.on('update', namespace='/controller')
 def move(message):
     print(message)
-    socketio.emit('movement',
-                  {'data': message['cmd']},
-                  namespace='/projection')
+    socketio.emit('movement', {'data':message['cmd']}, namespace='/projection')
