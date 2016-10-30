b7d29ca5e12175671d3e4eedd4abafe404dd962a
Abhik Pal <theabhikpal@gmail.com>
2016-10-30 10:52:33 +0530

    added processingJS support for projection

--- biennale/control.py
+++ biennale/control.py
@@ -1,8 +1,15 @@
+from flask import request
+
 from biennale import socketio
 
 @socketio.on('update', namespace='/controller')
 def move(message):
-    print(message)
+    # print(message)
+    # print(request.remote_addr)
     socketio.emit('movement',
-                  {'x':message['x'], 'y':message['y']},
-                  namespace='/projection')
+                    {
+                        'x':message['x'],
+                        'y':message['y'],
+                        'client':request.remote_addr
+                    },
+                    namespace='/projection')
